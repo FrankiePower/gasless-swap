dd4ed4f4d54c5ec02c53b63238f4086a3c87e592
SuperFranky <franklin.power21@gmail.com>
2025-06-11 16:08:28 +0100

    updated header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -87,13 +87,12 @@ export const Header = () => {
             <HeaderMenuLinks />
           </ul>
         </details>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
+        <Link href="/" passHref className="hidden lg:flex items-center ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="Swap logo" className="cursor-pointer" fill src="/swap.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Gasless Swap</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
