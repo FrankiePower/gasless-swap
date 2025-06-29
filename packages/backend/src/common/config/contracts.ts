693ff41e6e880899acf05442e7de2c28898c32bd
SuperFranky <franklin.power21@gmail.com>
2025-06-30 00:25:38 +0100

    backend contract setup

--- packages/backend/src/common/config/contracts.ts
+++ packages/backend/src/common/config/contracts.ts
@@ -4,7 +4,7 @@ type ContractConfig = {
 
 export const contract: ContractConfig = {
   // Sepolia testnet - the only chain currently supported
-  "11155111": "0x571B4228e412fC2ebaA209a75651BDE93ce4B4c2",  // SwapContract
+  "11155111": "0xE4494FbE3fbfdF89F408CB6c8cF9A50b213D4373",  // SwapContract
 };
 
 export const getContract = (chain: string) => {
