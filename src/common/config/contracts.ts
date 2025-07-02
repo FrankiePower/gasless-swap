358b34b6073c03c2c069f2979198176f6c3574eb
SuperFranky <franklin.power21@gmail.com>
2025-07-02 09:31:31 +0100

    updated contracts.ts

--- src/common/config/contracts.ts
+++ src/common/config/contracts.ts
@@ -14,4 +14,12 @@ export const getContract = (chain: string) => {
   }
   return contractAddress;
 };
+
+export const buildguidlToken = {
+  "11155111": "0x341b3060c5dc9bdbbb3e6d1f01b09c1a5b76d22c" // Replace with actual address
+};
+
+export const superToken = {
+  "11155111": "0x9359c395ef76af7a17e46b6f559ce0997fec31e3" // Replace with actual address
+};
   
